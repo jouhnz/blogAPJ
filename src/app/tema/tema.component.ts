a08c28ea7281d4b2f9979c7a41a6f25721582bab
unknown <jonathansoliveira7@gmail.com>
2022-11-25 19:01:53 -0300

    Componente tema sem consumo

--- src/app/tema/tema.component.ts
+++ src/app/tema/tema.component.ts
@@ -1,20 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { environment } from 'src/environments/environment.prod';
-
 @Component({
-  selector: 'app-inicio',
-  templateUrl: './inicio.component.html',
-  styleUrls: ['./inicio.component.css']
+  selector: 'app-tema',
+  templateUrl: './tema.component.html',
+  styleUrls: ['./tema.component.css']
 })
-export class InicioComponent implements OnInit {
+export class TemaComponent implements OnInit {
 
   constructor(
     private router: Router
   ) { }
 
   ngOnInit() {
-    
     if(environment.token == ''){
       this.router.navigate(['/entrar'])
     }
