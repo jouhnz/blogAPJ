3608b1fa3c8151dcaf90fa1edc5e8022de110539
jouhnz <jonathansoliveira7@gmail.com>
2023-02-02 16:43:39 -0300

    Código finalizado

--- src/app/service/tema.service.ts
+++ src/app/service/tema.service.ts
@@ -25,7 +25,7 @@ return this.http.get<Tema>(`http://localhost:8080/tema/${id}`, this.token)
 }
 
 getByNomeTema(nome: string): Observable<Tema[]>{
-  return this.http.get<Tema[]>(`https://localhost:8080/tema/nome/${nome}`, this.token)
+  return this.http.get<Tema[]>(`http://localhost:8080/tema/nome/${nome}`, this.token)
 }
 
 
